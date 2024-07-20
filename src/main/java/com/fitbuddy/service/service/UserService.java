6fb8873fc9925da03483d713e0cba21f48b53590
newkayak12 <newkayak12@gmail.com>
2024-07-20 17:09:48 +0900

    service

---
+++ src/main/java/com/fitbuddy/service/service/UserService.java
@@ -0,0 +1,9 @@
+package com.fitbuddy.service.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserService {
+}
