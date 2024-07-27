c4020d78ea7ad7e346388587f6b9fd3aeb031f0c
newkayak12 <newkayak12@gmail.com>
2024-07-27 20:45:15 +0900

    ADD prepareTest

---
+++ src/test/java/com/fitbuddy/service/service/UserServiceTest.java
@@ -0,0 +1,16 @@
+package com.fitbuddy.service.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class UserServiceTest {
+
+    @Nested
+    @DisplayName("")
+    class VerifyTest {
+
+    }
+}
